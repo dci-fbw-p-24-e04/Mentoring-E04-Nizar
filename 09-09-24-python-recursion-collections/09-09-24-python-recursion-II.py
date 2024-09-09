f5f4c47a35ad8e4733447aa2c534cfd7cd1dc4a7
Nizar Chaouachi <nizar.chawechy.1618@gmail.com>
2024-09-09 11:46:13 +0200

    final commit of the day

---
+++ 09-09-24-python-recursion-collections/09-09-24-python-recursion-II.py
@@ -0,0 +1,15 @@
+def login(counter=3):
+    username = input("username : ")
+    password = input("password : ")
+    if username != 'admin' or password!='password':
+        print('wrong user data')
+        counter -= 1
+        print(f"you still have {counter} tries left ")
+        if counter == 0:
+            print("you have attemped all your tries, are you trying to hack this account ?")
+            return 
+        login(counter)
+    else:
+        print('welcome admin')
+        
+login()
