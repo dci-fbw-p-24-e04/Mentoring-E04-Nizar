09dd25c98127eac2980650bec8c590094efa50aa
Nizar Chaouachi <nizar.chawechy.1618@gmail.com>
2024-09-09 11:19:52 +0200

    recursion

---
+++ 09-09-24-python-recursion-collections/09-09-24-python-recursion.py
@@ -0,0 +1,65 @@
+list_of_nums = [1, 2, 3, 4, 5, 6, 8, 4, 1, 2, 3, 6, 9, 4]
+#do not use sum() or loops here
+def sum_l(lst):
+    #break case : the function won't call itself again
+    if len(lst) == 0: # or if not lst
+        return 0
+    else :
+        return lst[0] + sum_l(lst[1:])
+'''
+[1, 2, 3, 4, 5, 6, 8, 4, 1, 2, 3, 6, 9, 4]
+1+[2, 3, 4, 5, 6, 8, 4, 1, 2, 3, 6, 9, 4]
+1+2+[3, 4, 5, 6, 8, 4, 1, 2, 3, 6, 9, 4]
+[4, 5, 6, 8, 4, 1, 2, 3, 6, 9, 4]
+[5, 6, 8, 4, 1, 2, 3, 6, 9, 4]
+[6, 8, 4, 1, 2, 3, 6, 9, 4]
+[8, 4, 1, 2, 3, 6, 9, 4]
+[4, 1, 2, 3, 6, 9, 4]
+[1, 2, 3, 6, 9, 4]
+[2, 3, 6, 9, 4]
+[3, 6, 9, 4]
+[6, 9, 4]
+[9, 4]
+[4]
+[]
+58'''  
+result = print(sum_l(list_of_nums))
+print(sum_l([]))
+print('-'*50)
+def factorial(n):
+    #n*(n-1)*(n-2)*.....*1 
+    #5! = 120
+    #0! = 1
+    if n == 0:
+        return 1
+    else :
+        return n*factorial(n-1)
+print(factorial(5))
+print(factorial(6))
+print('-'*50)
+def fibonacci(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+#1,1,2,3,5,8,13,21
+print(fibonacci(9))
+print('-'*50)
+
+def is_palindrome(string):
+    #level
+    #racecar
+    #bob
+    #return string == string[::-1]
+    if len(string)<=1:
+        return True
+    if string[0] == string[-1]:
+        return is_palindrome(string[1:-1])
+    else :
+        return False
+    
+print('racecar',is_palindrome('racecar'))
+print('level',is_palindrome('level'))
+print('pythop',is_palindrome('pythop'))
