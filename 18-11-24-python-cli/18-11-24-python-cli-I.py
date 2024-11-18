cf24a4b6e0e29b92056294b49abe2d22621e8289
Nizar Chaouachi <nizar.chawechy.1618@gmail.com>
2024-11-18 13:38:52 +0100

    cli 18/11/24-I

--- 18-11-24-python-cli/18-11-24-python-cli-I.py
+++ 18-11-24-python-cli/18-11-24-python-cli-I.py
@@ -2,8 +2,3 @@ import os
 
 os.system("sudo apt update")
 os.system("touch text.txt")
-import random
-#do not run this code
-num = random.randint(1,6)
-if num == 4:#russian roulette
-    os.system("os reboot")
